428aad8ca2561c27587643cb95c6863961da5a11
Paul Nykiel <paul.nykiel@gmail.com>
2022-12-09 20:06:04 +0100

    Ported JSON to xml for multiline text

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-REQ_JSONS := $(shell find chapters/02_requirements -name '*.json')
-REQ_TEXS := $(subst json,tex,${REQ_JSONS})
+REQ_XMLS := $(shell find chapters/02_requirements -name '*.xml')
+REQ_TEXS := $(subst xml,tex,${REQ_XMLS})
 DOTS := $(shell find . -name '*.dot')
 EPSS := $(subst dot,eps,${DOTS})
 TEXS := $(shell find . -name '.tex')
@@ -7,10 +7,9 @@ TEXS := $(shell find . -name '.tex')
 all: main.pdf
 
 main.pdf: $(TEXS) $(EPSS) $(REQ_TEXS)
-	echo "${REQ_TEXS}"
 	latexmk -pdf -d main.tex
 
-%.tex: %.json
+%.tex: %.xml
 	python3 tools/generate_tex.py $< $@
 
 %.eps: %.svg
--- tools/generate_tex.py
+++ tools/generate_tex.py
@@ -1,5 +1,5 @@
-import json
 import sys
+import xml.etree.ElementTree as ET
 
 
 def main():
@@ -7,35 +7,39 @@ def main():
         print(f"Usage {sys.argv[0]} in.json out.tex")
         exit(1)
 
-    in_json = sys.argv[1]
+    in_xml = sys.argv[1]
     out_tex = sys.argv[2]
 
-    with open(in_json, "r") as in_file:
-        contents = "\n".join(in_file.readlines())
-        root = json.loads(contents)
+    tree = ET.parse(in_xml)
+    root = tree.getroot()
 
     with open(out_tex, "w") as out_file:
-        out_file.write(f"\\group{{{root['title']}}}{{{root['id']}}}\n\n")
+        title = root.attrib["title"]
+        id = root.attrib["id"]
+        out_file.write(f"\\group{{{title}}}{{{id}}}\n\n")
 
-        for req in root["requirements"]:
-            type = req["type"]
-            id = req["id"]
-            title = req["title"]
-            descr = req["descr"]
+        for req in root:
+            id = req.attrib["id"]
+            type = req.attrib["type"]
+            title = req.find("title").text
+            descr = req.find("description").text
 
-            if req["type"] == "requirement":
+            if type == "requirement":
                 out_file.write(f"\\req{{{id}}}{{{title}}}\n"
                                f"{{{descr}}}\n"
-                               f"{{\n")
-                if "parent" in req:
-                    for parent in req["parent"]:
-                        out_file.write(f"\\parent{{{parent}}} ")
+                               f"{{")
+                parents = req.find("parents")
+                if parents is not None:
+                    for parent in parents:
+                        out_file.write(f"\\parent{{{parent.text}}} ")
                 
-                partition = req["partition"] if "partition" in req else ""
+                partition = req.find("partition")
+                partition = partition.text if partition is not None else ""
                 out_file.write(f"}}\n"
                                f"{{{partition}}}\n\n")
             else:
-                reason = req['reason']
+                print(id, req.tag)
+                reason = req.find('reason').text
                 out_file.write(f"\\dd{{{id}}}{{{title}}}\n"
                                f"{{{descr}}}\n"
                                f"{{{reason}}}\n\n")
